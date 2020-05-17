274d1ce501324ef2822996fc0fdee4b85c94f74d
Joaquin Martinez <joaco1509@gmail.com>
2020-05-17 19:50:26 -0300

    commit

--- main.py
+++ main.py
@@ -2,17 +2,18 @@ import time
 import datetime as dt
 import psutil
 
+#Este programa sirve para que no se pueda usar steam entre la 1 am y las 5 am
+
 if __name__ == '__main__':
 
     hora_inicial = 0
-    hora_final = 0
+    hora_final = 5
     nombre_proceso = "steam.exe"
 
     while 1:
-        ##os.system("TASKKILL /F /IM notepad.exe")
-        time.sleep(10)#wait 3 minutes
+
+        time.sleep(180)#wait 3 minutes
         hora = dt.datetime.now().hour
-        #print(hora)
 
         if hora_inicial < hora < hora_final: #ejecuta entre hora_inicial y hora_final
             for process in psutil.process_iter():
